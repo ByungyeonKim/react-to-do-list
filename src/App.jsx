2102414555e91a0f8f7f44767a2460b15d366961
ByungyeonKim <viva_kby@naver.com>
2024-01-30 22:16:32 +0900

    Create TodoList, TodoItem, Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
+
 import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
 
 function App() {
+  // {
+  //   id: '',
+  //   title: '',
+  //   contents: '',
+  //   isDone: false,
+  // }
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
       <h1>오늘의 할 일 목록</h1>
       <TodoForm />
+      <h2>오늘 할 일</h2>
+      <TodoList todos={todos} />
+      <h2>완료한 일</h2>
+      <TodoList todos={todos} />
+      <Footer />
     </>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import { TodoItem } from './TodoItem';
+
+export function TodoList({ todos }) {
+  if (!todos.length) return <p>오늘은 비어있어요.</p>;
+
+  return (
+    <ul>
+      {todos.map((todo) => {
+        return (
+          <li>
+            <TodoItem />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
